eea51c0a426de1030e8197b09d73110b8a974ef4
julio <juliochoque273@gmail.com>
2025-08-21 21:50:09 -0400

    BD conectada

---
+++ examen-final/database/seeders/CategoriaSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+        ['name' => 'Entradas', 'description' => 'Aperitivos y entradas'],
+        ['name' => 'Platos Principales', 'description' => 'Platos fuertes'],
+        ['name' => 'Postres', 'description' => 'Deliciosos postres'],
+        ['name' => 'Bebidas', 'description' => 'Refrescos y jugos'],
+    ];
+
+    // foreach ($categories as $category) {
+    //     Categoria::create($category);
+    // }
+    }
+}
